24094c05f5a3d6ca9f12bb0073725793d2b05f04
Nathaniel Erwin <erwinn@ci.newark.nj.us>
2020-11-20 18:34:11 -0500

    Auto-generated commit

--- ng-maps/lib/content.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class ContentComponent {
-    elementRef: ElementRef;
-    constructor(elementRef: ElementRef);
-    static ɵfac: i0.ɵɵFactoryDef<ContentComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDefWithMeta<ContentComponent, "map-content", never, {}, {}, never, ["*"]>;
-}
